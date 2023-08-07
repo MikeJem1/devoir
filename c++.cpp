b64f29407fbe3fc33925483b7c7b2388e2e99441
MikeJem1 <104571076+MikeJem1@users.noreply.github.com>
2023-08-07 11:44:12 -0400

    Add files via upload

---
+++ c++.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int a,b,s;
+
+int main()
+{
+	cout<<"Entrer un entier:";
+	cin>> a;
+	cout<<"Entrer un  autre entier:";
+	cin>>b;
+	s= a+b;
+	cout<<"\n la somme des deux entiers est:"<<s;
+	
+	return 0;
+}
